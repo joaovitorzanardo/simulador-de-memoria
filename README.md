6affc1bffccd4419e58eccb181e5912b38e5f844
joaovitorzanardo <joaovizan@hotmail.com>
2023-11-18 17:41:15 -0300

    Alterações

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Simulador de Memória
+
+* Para executar o programa é necessário entrar na pasta do projeto através de um terminal e executar o seguinte comando: ``python main.py``
--- main.py
+++ main.py
@@ -1,31 +1,104 @@
-import os
+import glob, os, random, time
 
-def loadFile(file_name):
+def showAllFileNames():
+    print("Arquivos Disponíveis")
+    for file in glob.glob("*.txt"):
+        print(file)
+
+def getFile(fileName):
+    file = None
     try:
-        file = open(file_name)
-        print(file.readline()) 
+        file = open(fileName) 
     except FileNotFoundError:
-        print(f"Arquivo {file_name} não encontrado.")    
+        print(f"Arquivo {fileName} não encontrado.")
+    return file
 
-def searchInFile(string):
-    print('String Digitada: ' + string)
+def loadFileIntoRam(file):
+    ram = []
+    for line in file:
+        ram.append(line.strip())
+    random.shuffle(ram)    
+    return ram
+
+def loadFileIntoSam(file):
+    sam = []
+    for line in file:
+        sam.append(line.strip())
+    return sam      
+
+def searchStringinSam(string):
+    startTime = time.perf_counter()
+    found = False
+    for w in sam:
+        if (w == string):
+            found = True
+            endTime = time.perf_counter()
+            break
+    if (found):
+        elapsedTime = (endTime - startTime) * 1000
+        print(f"Tempo para pesquisar na SAM: {elapsedTime:.6f}")    
+    else:
+        print(f"String {string} não encontrada na memória!")
+
+def searchStringinRam(string):
+    startTime = time.perf_counter()
+    found = False
+    for w in ram:
+        if (w == string):
+            found = True
+            endTime = time.perf_counter()
+            break
+    if (found):
+        elapsedTime = (endTime - startTime) * 1000
+        print(f"Tempo para pesquisar na RAM: {elapsedTime:.6f}") 
+    else:
+        print(f"String {string} não encontrada na memória!")     
+                    
+def searchStringInFile(string):
+    searchStringinRam(string)
+    searchStringinSam(string)
+    
 
 print('[1] Carregar Arquivo')
 print('[2] Buscar Informação')
 print('[3] Encerrar Programa')
 
-op = 0
+opInt = 0
 
-while (op != 3):
+sam = []
+ram = []
+
+while (opInt != 3):
     op = input('Digite a opção desejada: ')
 
-    if (int(op) == 1):
-        file_name = input('Digita o nome do arquivo que deseja carregar: ')
-        loadFile(file_name)
-    elif (int(op) == 2):
-        string = input('Digita a string que deseja pesquisar: ')
-        searchInFile(string)
-    elif (int(op) == 3):
+    try:
+        opInt = int(op)
+    except Exception:
+        print('Opção Inválida')
+        continue     
+
+    if (opInt == 1):
+        showAllFileNames()
+        fileName = input('Digita o nome do arquivo que deseja carregar: ')
+        file = getFile(fileName)
+        if (file is None):
+            continue
+        ram = loadFileIntoRam(file)
+        file.seek(0)
+        sam = loadFileIntoSam(file)
+        file.close()
+        if (len(ram) > 0 and len(sam) > 0):
+            print(f"Arquivo {fileName} carregado na memória com sucesso!")
+        else:
+            print(f"O arquivo {fileName} não foi carregado na memória corretamente!")    
+    elif (opInt == 2):
+        if (len(ram) > 0 and len(sam) > 0):
+            string = input('Digita a string que deseja pesquisar: ')
+            searchStringInFile(string) 
+        else:
+            print('Nenhum arquivo carregado na memória. Carregue um arquivo antes de fazer uma busca!')    
+    elif (opInt == 3):
+        print('Encerrando Programa!')
         break;    
     else:
         print('Opção Inválida')
